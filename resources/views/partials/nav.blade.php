a35c2204c3472a3efd09729c3785c0af2932540b
Sabatino Masala <Masalasabatino@gmail.com>
2020-03-05 21:45:42 +0100

    Add partial

---
+++ resources/views/partials/nav.blade.php
@@ -0,0 +1,15 @@
+<nav class="navbar navbar-light bg-light shadow">
+    <a class="navbar-brand" href="#">Snackbar</a>
+
+    <ul class="navbar-nav mr-auto">
+        <li class="nav-item active">
+            <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
+        </li>
+    </ul>
+    <div>
+        <button class="btn btn-secondary">
+            <i class="fa fa-shopping-cart"></i>
+            <span class="badge badge-pill badge-primary">Amount: 0</span>
+        </button>
+    </div>
+</nav>
