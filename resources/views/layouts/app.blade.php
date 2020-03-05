3b84a5a6393cda7da7be689fd20bf8a454b60456
Sabatino Masala <Masalasabatino@gmail.com>
2020-03-05 21:49:38 +0100

    Cleanup app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,11 +4,9 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Snackbar</title>
 
         <link rel="stylesheet" href="{{ mix('css/app.css') }}">
-
-        @livewireStyles
     </head>
     <body>
 
@@ -21,7 +19,6 @@
         </div>
 
         <script src="{{ mix('js/app.js') }}"></script>
-        @livewireScripts
-        @stack('scripts')
+
     </body>
 </html>
