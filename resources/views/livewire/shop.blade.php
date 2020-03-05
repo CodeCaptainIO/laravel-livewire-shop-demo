6a0ee36903b50affbd5261c8456a8b4b23538772
Sabatino Masala <Masalasabatino@gmail.com>
2020-03-05 21:44:26 +0100

    Add nav to partial

--- resources/views/livewire/shop.blade.php
+++ resources/views/livewire/shop.blade.php
@@ -1,27 +1,5 @@
 <div x-data="initialData()" x-init="init()">
-    <nav class="navbar navbar-light bg-light shadow">
-        <a class="navbar-brand" href="#">Snackbar</a>
-
-        <ul class="navbar-nav mr-auto">
-            <li class="nav-item active">
-                <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
-            </li>
-        </ul>
-        <div>
-            <button class="btn btn-secondary" @click="cartVisible = true">
-                <i class="fa fa-shopping-cart"></i>
-                <span class="badge badge-pill badge-primary">Amount: {{ $total }}</span>
-            </button>
-        </div>
-        <div class="card shadow cart" x-show.transition="cartVisible" @click.away="cartVisible = false">
-            <div class="card-header">
-                Your cart
-            </div>
-            <div class="card-body">
-                <livewire:cart />
-            </div>
-        </div>
-    </nav>
+    @include('partials.nav')
     <div class="container mt-5">
         <livewire:product-list :products="$products" />
     </div>
